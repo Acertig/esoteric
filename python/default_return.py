2af5d2d9e245b803604433a070b3c2da3e113eb8
mercer <67067768+Acertig@users.noreply.github.com>
2021-11-18 14:33:48 -0500

    Create default_return.py

---
+++ python/default_return.py
@@ -0,0 +1,16 @@
+
+def default_return(value = None): 
+    def factory(func): 
+        def inner(*args, **kwargs): 
+            if func(*args, **kwargs) is None: 
+                func.__code__ = func.__code__.replace(co_consts = (value, *func.__code__.co_consts[1::]))
+                return func(*args, **kwargs)
+            return func(*args, **kwargs)
+        return inner
+    return factory
+
+@default_return(value = "acertig")
+def test(): 
+    pass
+
+print(test())
