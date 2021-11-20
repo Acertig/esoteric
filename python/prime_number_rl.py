d36dc601c6150019be4648ff3b801847235bd025
mercer <67067768+Acertig@users.noreply.github.com>
2021-11-20 14:30:42 -0500

    Update prime_number_rl.py

--- python/prime_number_rl.py
+++ python/prime_number_rl.py
@@ -1,1 +1,1 @@
-sp = int(input());print((lambda n,i=2:True if(n<=2) else(False if (n%i==0)else(True if(i*i>n)else type(lambda:0)((__import__("sys")._getframe(0).f_code),globals(),"death")(n,i+1))))(sp))
+sp=int(input());print((lambda n,i=2:True if(n<=2) else(False if (n%i==0)else(True if(i*i>n)else type(lambda:0)((__import__("sys")._getframe(0).f_code),globals(),"death")(n,i+1))))(sp))
